619a02441c581a88fb26650bc517e6445350f476
simyikong <s2177224@siswa.um.edu.my>
2024-05-24 00:58:23 +0800

    added database overview

---
+++ src/app/overview/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Overview: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold">Overview Page</h1>
+      <p>Database Overview...</p>
+    </div>
+  );
+};
+
+export default Overview;
